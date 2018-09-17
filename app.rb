aec3db6143ef2d6b299324404196efd257bf47b9
github username <github email address>
2018-09-17 10:47:55 -0400

    Done.

--- app.rb
+++ app.rb
@@ -2,8 +2,8 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
-  get '/' do 
-    "Welcome to your app!!!! "
+  get '/' do
+    "Welcome to your app!!!! STEVEN CHILLING Yo testing testing "
   end
 
 end
